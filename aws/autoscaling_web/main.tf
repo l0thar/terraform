37c77dede54cab243349968dd4e1a97a33db58f8
Kevin Lambert <kevinrlambert@gmail.com>
2019-08-07 09:09:17 -0600

    Adjust formatting, add elb_port variable

--- aws/autoscaling_web/main.tf
+++ aws/autoscaling_web/main.tf
@@ -9,7 +9,12 @@ data "aws_availability_zones" "available" {
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  default = 8080
+  default     = 8080
+}
+
+variable "elb_port" {
+  description = "The port the web server will use for HTTP requests"
+  default     = 80
 }
 
 resource "aws_launch_configuration" "example" {
@@ -47,8 +52,8 @@ resource "aws_security_group" "elb" {
   name = "terraform-example-elb"
 
   ingress {
-      from_port     = 80
-      to_port       = 80
+      from_port     = "${var.elb_port}"
+      to_port       = "${var.elb_port}"
       protocol      = "tcp"
       cidr_blocks   = ["0.0.0.0/0"]
   }
@@ -62,8 +67,8 @@ resource "aws_security_group" "elb" {
 }
 
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = "${aws_launch_configuration.example.id}"
-  availability_zones = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}"]
+  launch_configuration  = "${aws_launch_configuration.example.id}"
+  availability_zones    = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}"]
 
   load_balancers    = ["${aws_elb.example.name}"]
   health_check_type = "ELB"
@@ -81,17 +86,17 @@ resource "aws_autoscaling_group" "example" {
 resource "aws_elb" "example" {
   name                  = "terraform-asg-example"
   availability_zones    = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}"]
-  security_groups = ["${aws_security_group.elb.id}"]
+  security_groups       = ["${aws_security_group.elb.id}"]
 
   listener {
-      lb_port           = 80
+      lb_port           = "${var.elb_port}"
       lb_protocol       = "http"
       instance_port     = "${var.server_port}"
       instance_protocol = "http"
   }
 
   health_check {
-      healthy_threshold = 2
+      healthy_threshold     = 2
       unhealthy_threshold   = 2
       timeout               = 3
       interval              = 30
