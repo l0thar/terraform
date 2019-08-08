91d5c54ea72b975f4e0cbd1115e22210425b2f1f
Kevin Lambert <kevinrlambert@gmail.com>
2019-08-08 14:45:36 -0600

    Updated backend_s3 formatting

--- aws/03_backend_s3/main.tf
+++ aws/03_backend_s3/main.tf
@@ -15,9 +15,9 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
-  name = "YOUR_DYNAMODB_TABLE_NAME"
-  hash_key = "LockID"
-  read_capacity = 5
+  name           = "YOUR_DYNAMODB_TABLE_NAME" # MUST BE REGIONALLY UNIQUE!
+  hash_key       = "LockID"
+  read_capacity  = 5
   write_capacity = 5
 
   attribute {
@@ -30,16 +30,16 @@ resource "aws_dynamodb_table" "terraform_lock" {
   }
 }
 
-# Add the following in a .tf file in your project directory (needs to be in at least one)
+# After the bucket and DynamoDB table are created, add the following in a .tf file in your project directory (needs to be in at least one)
 # If added to an existing directory where you've run `terraform init` previously, it will
 # tell you to run the init command again to update to the new backend.
 #
-#terraform {
-#  backend "s3" {
-#    encrypt=true
-#    bucket="YOUR_BUCKET_NAME"
-#    key="<path>/terraform.tfstate"
-#    region="us-east-1"
-#    dynamodb_table = "YOUR_DYNAMODB_TABLE_NAME"
-#  }
-#}
+# terraform {
+#   backend "s3" {
+#     encrypt        = true
+#     bucket         = "YOUR_BUCKET_NAME"
+#     key            = "<path>/terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "YOUR_DYNAMODB_TABLE_NAME"
+#   }
+# }
